c79d18df6fca88d0c5c5afd45f8347e5e993d8ab
XiaoJian <demon_xiaojian@163.com>
2016-07-30 20:19:46 +0800

    Add "Leetcode 27 : Remove Element" problem.

---
+++ RemoveElement/src/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ * @author Demon 2016.7.30
+ * 
+ *         Given an array and a value, remove all instances of that value in
+ *         place and return the new length.
+ * 
+ *         Do not allocate extra space for another array, you must do this in
+ *         place with constant memory.
+ * 
+ *         The order of elements can be changed. It doesn't matter what you
+ *         leave beyond the new length.
+ * 
+ *         Example: Given input array nums = [3,2,2,3], val = 3
+ * 
+ *         Your function should return length = 2, with the first two elements
+ *         of nums being 2.
+ *
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.removeElement(new int[] { 3, 2, 2, 3 }, 3));
+	}
+
+	public int removeElement(int[] nums, int val) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != val) {
+				nums[count++] = nums[i];
+			}
+		}
+		return count;
+	}
+
+}
